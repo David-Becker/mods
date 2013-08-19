305244907811f9e986e30bda3f67c90cb76cf0fc
David-Becker <email@git>
2013-08-19 21:00:02 +0200

    Updated LSOS to version 5

--- Light Side Of Spring/LuaRules/Gadgets/reflection.lua
+++ Light Side Of Spring/LuaRules/Gadgets/reflection.lua
@@ -15,12 +15,18 @@ local guidelaser = WeaponDefNames.guidelaser.id
 
 local reflective= {
 	[UnitDefNames.mirror.id]=true,
+	[UnitDefNames.splitter.id]=true,
 	[UnitDefNames.bottle.id]=true,
 }
 
 if (gadgetHandler:IsSyncedCode()) then
 
 --SYNCED
+
+local timeout={}
+local timeToNeutral=150
+
+local transferList={}
 
 function gadget:UnitDamaged(u, ud, team, damage, para, weapon, attacker, au, ateam)
 	if reflective[ud] and attacker then
@@ -33,7 +39,37 @@ function gadget:UnitDamaged(u, ud, team, damage, para, weapon, attacker, au, ate
 			Spring.SetUnitHealth(u, {health = math.min(health + (damageReduction/100.0 * damage), maxHealth)})
 		else
 			Spring.CallCOBScript(u,"AimLaser",0,heading)
+			timeout[u]=Spring.GetGameFrame()+timeToNeutral
+			if team == Spring.GetGaiaTeamID() then
+				transferList[u]=ateam
+			end
 		end
+	end
+end
+
+function gadget:UnitFinished(u, ud, team)
+	if reflective[ud] then
+		timeout[u]=Spring.GetGameFrame()+timeToNeutral
+	end
+end
+
+function gadget:UnitDestroyed(u, ud, team)
+	timeout[u]=nil
+end
+
+function gadget:GameFrame(f)
+	if f%32 < .1 then
+		for u,t in pairs(timeout) do
+			if t < f then
+				transferList[u]=Spring.GetGaiaTeamID()
+				timeout[u]=nil
+			end
+		end
+	end
+	for u,t in pairs(transferList) do
+		Spring.TransferUnit(u, t, false)
+		timeout[u]=nil
+		transferList[u]=nil
 	end
 end
 
